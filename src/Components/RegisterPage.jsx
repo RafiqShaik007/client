d7b70c2e78def0ccfb67b4ead28961348dbc1810
RafiqShaik007 <shaikrafiq9052707297@gmail.com>
2025-02-27 23:22:00 +0530

    "Register form submittion"

--- src/Components/RegisterPage.jsx
+++ src/Components/RegisterPage.jsx
@@ -1,14 +1,18 @@
 import "../styles/register.css"
 import { useState } from "react";
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import axios from 'axios'
 
 const RegisterForm = () => {
-  const [formData, setFormData] = useState({ fullname: "", email: "", password: "" });
+  const [fullname, setFullname] = useState()
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+
   const [showPassword, setShowPassword] = useState(false);
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.id]: e.target.value });
-  };
+  let navigate = useNavigate()
+
+ 
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
@@ -16,28 +20,43 @@ const RegisterForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Form submitted", formData);
+    console.log("Form submitted", fullname, email, password);
+    axios.post('http://localhost:2323/api/user/register', {fullname, email, password})
+    .then(function(res){
+      console.log(res)
+      if(res.status === 201 || res.status === 200){
+        console.log('Data is created successfully')
+        alert('User is Registered Successfully')
+        navigate('/login')
+      }
+      else{
+        alert('Failed to register... Try after some time')
+      }
+    })
+    .catch(function(){
+      console.log('Got some error while registering data')
+    })
   };
 
   return (
     <div id="RegisterForm">
         <div className="auth-container">
       <div className="logo">GourmetExpress</div>
-      <form className="auth-form" onSubmit={handleSubmit}>
+      <form className="auth-form" >
         <div className="input-group">
-          <input type="text" id="fullname" placeholder=" " value={formData.fullname} onChange={handleChange} required />
+          <input type="text" id="fullname" placeholder=" " value={fullname} onChange={(e)=>{setFullname(e.target.value)}}  required />
           <label htmlFor="fullname">Full Name</label>
         </div>
         <div className="input-group">
-          <input type="email" id="email" placeholder=" " value={formData.email} onChange={handleChange} required />
+          <input type="email" id="email" placeholder=" " value={email} onChange={(e)=>{setEmail(e.target.value)}}  required />
           <label htmlFor="email">Email Address</label>
         </div>
         <div className="input-group">
-          <input type={showPassword ? "text" : "password"} id="password" placeholder=" " value={formData.password} onChange={handleChange} required />
+          <input type={showPassword ? "text" : "password"} id="password" placeholder=" " value={password} onChange={(e)=>{setPassword(e.target.value)}}  required />
           <label htmlFor="password">Password</label>
           <i className={`fas ${showPassword ? "fa-eye-slash" : "fa-eye"} password-toggle`} onClick={togglePasswordVisibility}></i>
         </div>
-        <button type="submit" className="submit-btn">Create Account</button>
+        <button type="submit" className="submit-btn" onClick={handleSubmit}>Create Account</button>
         <div className="auth-switch">
           Already have an account? <Link to='/login'>Sign in</Link>
         </div>
